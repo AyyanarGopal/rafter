15f2b2a31642edf5a70c842eed0dfd4bb243952d
Ayyanar_Gopal <142037298+AyyanarGopal@users.noreply.github.com>
2023-10-22 05:26:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changeImage1() {
-    document.getElementById("banner").style = "background: url(../rafter/images/hero-2.jpg); background-size: cover;";
+    document.getElementById("banner").style = "background: url(./images/hero-2.jpg); background-size: cover;";
 }
 function changeImage2() {
-    document.getElementById("banner").style = "background: url(../rafter/images/hero-1-scaled.jpg); background-size: cover;";
+    document.getElementById("banner").style = "background: url(./images/hero-1-scaled.jpg); background-size: cover;";
 }
 function changeImage3() {
-    document.getElementById("banner").style = "background: url(../rafter/images/video-1.jpg); background-size: cover;";
+    document.getElementById("banner").style = "background: url(./images/video-1.jpg); background-size: cover;";
 }
