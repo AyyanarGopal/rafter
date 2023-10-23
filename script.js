38eb4ab70987d9cc75bfc8028330b1124d748da6
Ayyanar_Gopal <142037298+AyyanarGopal@users.noreply.github.com>
2023-10-22 21:28:47 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function changeImage1() {
-    document.getElementById("banner").style = "background: url(./images/hero-2.jpg); background-size: cover;";
-}
-function changeImage2() {
-    document.getElementById("banner").style = "background: url(./images/hero-1-scaled.jpg); background-size: cover;";
-}
-function changeImage3() {
-    document.getElementById("banner").style = "background: url(./images/video-1.jpg); background-size: cover;";
-}
